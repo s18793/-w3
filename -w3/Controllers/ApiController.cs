a9aa9c068d650399f0cc50b9595bc8978c5bee56
Paweł Pietrzak <s18793@pjwstk.edu.pl>
2020-03-19 12:05:54 +0100

    1

---
+++ -w3/Controllers/ApiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cwiczenia3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+
+    public class StudentsController : ControllerBase
+    {
+        public string GetStudent()
+        {
+            return "Kowalski, Malewski, Andrzejewski";
+        }
+    }
+}
